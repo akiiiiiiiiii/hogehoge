7e538942a5aa45f32fbafd60c22e1a92d4ba0aa1
中山明紀 <nakayama@regulus-technologies.co.jp>
2019-12-04 20:03:41 +0900

    ユーザー登録画面途中

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
-  def detail
+  def show
     @user = User.find_by(id: params[:id])
   end
+
+  def index
+    @users = User.all
+  end
+
 
   # GET /resource/sign_up
   # def new
@@ -45,9 +50,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     }
 
   devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
+    get "user/:id", :to => "users/registrations#show"
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
