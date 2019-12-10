662443e10151a89aa16a764cf45fc63e658c29ed
中山明紀 <nakayama@regulus-technologies.co.jp>
2019-12-10 23:47:12 +0900

    途中

---
+++ db/fixtures/01_books.rb
@@ -0,0 +1,360 @@
+Book.seed_once(:id) do |b|
+  b.title = 'アジャイルサムライ'
+  b.description = 'マスターセンセイと学ぶアジャイル開発の道動くソフトウェアを素早く開発するための「アジャイルソフトウェア開発手法」を、実際に導入するにはどうすればよいかを、豊富な図を使い親しみやすい言葉で解説しています。経験豊かな著者が具体的なノウハウをまとめた本書は、アジャイル開発を導入したいと考えている組織や人のための「現場のマニュアル」として役立ってくれることでしょう。'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'アジャイル プラクティス'
+  b.description = '現場に即した開発手法の手引きアジャイル(俊敏な、フットワークの軽い)開発の実践的な側面を解説した書籍。単なる開発手法の教科書ではなく、実際の開発現場から生まれたアドバイスや手引きを、具体例を用いて達人プログラマが伝える。原書は、2007年Software Develompent誌Jolt Awardで一般書籍部門のProductivity Award を獲得。'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'アマゾノミクス'
+  b.description = '◎米amazon 元チーフ・サイエンティスト◎
+ジェフ・ベゾスとともに買い物の常識を変えた
+科学者が明かす巨大データ企業の秘密。
+Facebook,Uber,Google,Airbnb驚愕の戦略!'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'アルバイトが辞めない職場の作り方'
+  b.description = 'もしかして、「辞められ店長」になってしまっていませんか?
+空前の人材不足時代だからこそ、採用した人材を大切にしないといけません
+アルバイトがやめたくなってしまう理由とは?
+辞めさせないためにはどうしたらいいのか?
+この一冊であなたの店の「人材・職場環境」を解決しましょう'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'アントレプレナーの教科書'
+  b.description = '変貌するアジア、停滞する日本経済
+成長ビジネスは「新興アジア」が舞台となる'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'イーロン・マスク'
+  b.description = '「世界を救う」ことを人生のテーマとし、人類の火星移住を本気で目指す、今最も注目の天才起業家、イーロン・マスク。
+どんな窮地に立ってもそれを乗り越え、世界を驚かせる結果を出し続けているマスクの在り方は、危ういながらも非常に人を惹きつけます。
+世界を変える男が何を考え、どう生きてきたのか。本書ではマスクの言葉とともに、彼の人物像に迫ります。'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '飲食店経営2019/1'
+  b.description = '対談 髙岡慎一郎氏×本誌編集長 毛利英昭
+インバウンドと人材活用をテーマに2019年の外食産業の動向を占う'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'エクストリームプログラミング'
+  b.description = 'アジャイル開発とは何だったのか、その原点を再考する新訳
+優れた技術力と良好な人間関係をもってしてソフトウェア開発を成功に導く、ケント・ベックによるXP(エクストリームプログラミング)のすべてを集約した名著 "Extreme Programming Explained: Embrace Change" の新訳版。アジャイル開発の原点を知る、必読の一冊です。'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '大型商談を成約に導く「SPIN」営業術'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'カスタマーサクセス サブスクリプション時代に求められる「顧客の成功」１０の原則'
+  b.description = '業績が上がらないのは、本当に不況のせいだけなのか?
+20年以上「営業のバイブル」として読み継がれる、
+大型商談の必須テクニック、
+「SPIN」式を網羅したオリジナル・テキスト。
+不況の厳しい荒波を突破する切り札に!'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '起業のファイナンス'
+  b.description = '起業家はもちろん、ベンチャーキャピタルやエンジェル、弁護士、司法書士、公認会計士、税理士など、
+起業家をサポートする人からもご好評をいただいた同名図書の増補改訂版。
+前作は、ベンチャー投資額が大幅に減少し続ける中で発行されましたが、その後のベンチャー生態系の急速な活性化により、元気なベンチャーが多数起業し、
+数十億円規模の増資を成功させるベンチャーも登場するようになりました。
+そこで、2015年に施行予定の改正会社法も反映させた、ベンチャーのコーポレートガバナンスを解説する章を新たに書き足すなど、
+起業にまつわる最新の情報を盛り込み、全編を大幅に書き換えました。'
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '起業のエクイティ・ファイナンス'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '逆説のスタートアップ思考'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '逆説のスタートアップ思考'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'サブスクリプション・マーケティング'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '事業構想2017/11'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '事業構想2018/1'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '失敗の化学'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'スタートアップ・ウェイ'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'スタートアップで働く'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'スタートアップで働くということ'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'ステージ別 ベンチャー企業の労務戦略'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '戦略的コールセンターの'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '続・インタフェースデザインの心理学'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'ダイレクト・リクルーティング'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '小さなチーム、大きな仕事'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '沈黙のwebマーケティング'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'チャレンジャーセールスモデル'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'デザインスクリプト'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'デザインの伝え方'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'なぜ最近の若者は突然辞めるのか'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '日本のベストベンチャー15社'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'ノンデザイナーズ・デザインブック'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '発想する会社！'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'ビジネスで活かすサービスデザイン'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'フェイスブック 不屈の未来戦略'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'フォントのふしぎ'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'マイクロサービスアーキテクチャ'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'マーケティングオートメーション導入の教科書'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '未来学者が教える予測の技術'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'マスタリングTCP/IP'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'リーンエンタープライズ'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'リーン開発の現場'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'リーン顧客開発'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'リーン・スタートアップ成長戦略'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'リーンブランディング'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'Air bnb'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'JavaScript:The Good Parts'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'LEAN ANALYTICS'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'LEAN UX'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'nginx実践入門'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'PARANOID SURVIVE'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'RPAの威力'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'RPA革命の衝撃'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'Ruby on Rails5'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'RUNNING LEAN'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'SHOE DOG'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'START UP STUDIO'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'THE MODEL'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'THINK WILD'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'UXの時代'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'UX×Biz Book'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'Webを支える技術'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '＠人事11'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '@人事12'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '@人事14'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = '10年つかえるSEOの基本'
+  b.description = ''
+end
+
+Book.seed_once(:id) do |b|
+  b.title = 'ライト、ついてますか'
+  b.description = ''
+end
